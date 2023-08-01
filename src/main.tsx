383018e6eaf2d64ebc97435cf7134852adaf9e76
prakashprabhukota <prakashprabhu399@gmail.com>
2023-08-01 17:00:03 +0530

    Hashrouter changes

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
-  <HashRouter  basename="/shopping-cart">
+  <HashRouter>
     <App />
   </HashRouter>
 );
