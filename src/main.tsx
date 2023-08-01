70ffff6bec01d8cc59ae4caadea736043065c860
prakashprabhukota <prakashprabhu399@gmail.com>
2023-08-01 16:56:40 +0530

    Hashrouter changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
-  <BrowserRouter basename="/shopping-cart">
+  <HashRouter  basename="/shopping-cart">
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
