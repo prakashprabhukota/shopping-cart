0486c026bcff3f3fb4cd8d5d6a9b1ac175f46946
prakashprabhukota <prakashprabhu399@gmail.com>
2023-08-01 15:44:47 +0530

    browserrouter changes

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/shopping-cart">
     <App />
   </BrowserRouter>
 );
